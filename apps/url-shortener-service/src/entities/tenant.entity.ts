4f6ee64aa83483a479d0ca57b1522a0d85c4ee2f
Bruno Reche <reche.dev.lab@gmail.com>
2025-06-29 20:54:38 -0300

    feat(tenant): Define Tenant entity schema

--- apps/url-shortener-service/src/entities/tenant.entity.ts
+++ apps/url-shortener-service/src/entities/tenant.entity.ts
@@ -1,1 +1,27 @@
-export class Tenant {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Url } from './url.entity';
+
+@Entity('tenants')
+export class Tenant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Url, (url) => url.tenant)
+  urls: Url[];
+}
