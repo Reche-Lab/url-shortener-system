4ca1be0f822cf6a26ff4b4ee5227284fd12210ad
Bruno Reche <reche.dev.lab@gmail.com>
2025-06-29 09:16:24 -0300

    feat(url): Register Url entity and module in main app module

--- apps/url-shortener-service/src/url-shortener-service.module.ts
+++ apps/url-shortener-service/src/url-shortener-service.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlShortenerServiceController } from './url-shortener-service.controller';
 import { UrlShortenerServiceService } from './url-shortener-service.service';
+import { Url } from './entities/url.entity'; // CORRIGIDO: Caminho da entidade Url
+import { UrlModule } from './url.module'; // CORRIGIDO: Caminho do UrlModule (está no mesmo nível)
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { UrlShortenerServiceService } from './url-shortener-service.service';
       username: 'user',
       password: 'password',
       database: 'url_shortener_db',
-      autoLoadEntities: true,
+      entities: [Url],
       synchronize: true,
     }),
+    UrlModule,
   ],
   controllers: [UrlShortenerServiceController],
   providers: [UrlShortenerServiceService],
