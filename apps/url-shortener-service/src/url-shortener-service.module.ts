773cd96f30af394e6fed4b27d69f0f1629f316d8
Bruno Reche <reche.dev.lab@gmail.com>
2025-06-29 20:57:35 -0300

    feat(tenant): Register Tenant entity and module in main app module

--- apps/url-shortener-service/src/url-shortener-service.module.ts
+++ apps/url-shortener-service/src/url-shortener-service.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlShortenerServiceController } from './url-shortener-service.controller';
 import { UrlShortenerServiceService } from './url-shortener-service.service';
-import { Url } from './entities/url.entity'; // CORRIGIDO: Caminho da entidade Url
-import { UrlModule } from './url.module'; // CORRIGIDO: Caminho do UrlModule (está no mesmo nível)
+import { Url } from './entities/url.entity';
+import { UrlModule } from './url.module';
+import { Tenant } from './entities/tenant.entity';
 import { TenantModule } from './tenant.module';
 
 @Module({
@@ -15,7 +16,7 @@ import { TenantModule } from './tenant.module';
       username: 'user',
       password: 'password',
       database: 'url_shortener_db',
-      entities: [Url],
+      entities: [Url, Tenant],
       synchronize: true,
     }),
     UrlModule,
