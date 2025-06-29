c65869935958c648926e3f9c55be7ca894e89a41
Bruno Reche <reche.dev.lab@gmail.com>
2025-06-28 22:09:11 -0300

    feat: Configure Docker for PostgreSQL and NestJS TypeORM connection in url-shortener-service

--- apps/url-shortener-service/src/url-shortener-service.module.ts
+++ apps/url-shortener-service/src/url-shortener-service.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlShortenerServiceController } from './url-shortener-service.controller';
 import { UrlShortenerServiceService } from './url-shortener-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'url_shortener_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [UrlShortenerServiceController],
   providers: [UrlShortenerServiceService],
 })
