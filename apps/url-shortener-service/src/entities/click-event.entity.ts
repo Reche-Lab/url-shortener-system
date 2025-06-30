ba108753d5c45de100bb967d320e24cfc05fea03
Bruno Reche <reche.dev.lab@gmail.com>
2025-06-29 21:13:46 -0300

    feat(click-event): Add tenantId column and Many-to-One relationship with Tenant to ClickEvent entity

---
+++ apps/url-shortener-service/src/entities/click-event.entity.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Url } from './url.entity'; // Importe a entidade Url
+import { Tenant } from './tenant.entity'; // Importe a entidade Tenant
+
+@Entity('click_events')
+export class ClickEvent {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Url, (url) => url.clickEvents)
+  @JoinColumn({ name: 'urlId' })
+  url: Url;
+
+  @Column({ type: 'uuid' })
+  urlId: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  tenantId: string | null;
+
+  @ManyToOne(() => Tenant)
+  @JoinColumn({ name: 'tenantId' })
+  tenant: Tenant;
+
+  @Column({ nullable: true })
+  ipAddress: string;
+
+  @Column({ type: 'text', nullable: true })
+  userAgent: string;
+
+  @CreateDateColumn()
+  clickedAt: Date;
+}
